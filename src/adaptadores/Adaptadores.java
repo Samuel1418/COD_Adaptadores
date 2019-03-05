d1d94e50ded8e040f6d912b894afb98c27b29953
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-03-05 14:20:28 +0100

    Listo

--- src/adaptadores/Adaptadores.java
+++ src/adaptadores/Adaptadores.java
@@ -3,12 +3,12 @@ package adaptadores;
 public class Adaptadores {
 
     public static void main(String[] args) {
-        MediaPlayer player = new MP3();
-        player.play("file.mp3");
-        player = new FormatAdapter(new MP4());
-        player.play("file.mp4");
+        MediaPlayer player = new MP3(); //Instanciamos objeto del tipo MP3 que tiene como interfaz MediaPlayer
+        player.play("file.mp3");  // Como es normal puede hacer el play del propio ojbeto
+        player = new FormatAdapter(new MP4()); //Ahora introducimos en player el format adapter de abajo con la clase MP4
+        player.play("file.mp4");  //Al tener player el metodo play de la interfaz MediaPlayer, ejecutamos el play aunque sea MP4
         player = new FormatAdapter(new VLC());
-        player.play("file.avi");
+        player.play("file.avi");  //Aquí hacemos lo mismo de arriba pero con VLC
     }
 
     private static class FormatAdapter implements MediaPlayer {
@@ -16,13 +16,13 @@ public class Adaptadores {
         private MediaPackage media;
 
         public FormatAdapter(MediaPackage m) {
-            media = m;
+            media = m; // creamos variable del constructor.
         }
 
         @Override
-        public void play(String filename) {
+        public void play(String filename) {   //creanos un metodo para play
             System.out.print("Using Adapter --> ");
-            media.playFile(filename);
+            media.playFile(filename); // 
         }
     }
 }
